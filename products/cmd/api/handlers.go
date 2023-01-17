c1978129a2e96c155d267e9f167f3bde2fcb5d3a
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-17 14:57:22 +0300

    create product hadlers - GET / POST

---
+++ products/cmd/api/handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"products/repo"
+)
+
+func (app *Config) GetProducts(w http.ResponseWriter, r *http.Request) {
+	res, err := app.Models.ProductEntry.All()
+
+	if err != nil {
+		app.errJSON(w, err)
+		return
+	}
+
+	response := jsonResponse{
+		Error:   false,
+		Message: "Products Endpoint",
+		Data:    res,
+	}
+	app.writeJSON(w, http.StatusOK, response)
+}
+
+func (app *Config) AddProduct(w http.ResponseWriter, r *http.Request) {
+	var reqBody repo.ProductEntry
+
+	err := app.readJSON(w, r, &reqBody)
+
+	if err != nil {
+		app.errJSON(w, err)
+		return
+	}
+	err = app.Models.ProductEntry.Insert(reqBody)
+	if err != nil {
+		app.errJSON(w, err)
+		return
+	}
+	response := jsonResponse{
+		Error:   false,
+		Message: "Product created successfully",
+	}
+	app.writeJSON(w, http.StatusOK, response)
+}
